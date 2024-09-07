30b8959f9c84c99340cb3906d23034c52b9fc0d9
M7Tr0_ <mechanozver@gmail.com>
2024-09-07 18:32:19 +0800

    Enabled the sendMessage endpoint functionality. Now you can send mesages that will be saved. And a conversation will also be created/updated

--- chatApp/backend/controllers/message.controller.js
+++ chatApp/backend/controllers/message.controller.js
@@ -1,8 +1,30 @@
+import Conversation from '../models/Conversation.model.js';
+import Message from '../models/Message.model.js';
+
 export const sendMessage = async (req,res) => {
     try{
-        res.send("You reached sendMEssage");
+        const {id:receiverId} = req.params;
+        const senderId = req.userId;
+        const {message} = req.body;
+
+        let conversation = await Conversation.findOne({participants: {$all: [senderId, receiverId]}});
+        if(!conversation){
+            conversation = await new Conversation({
+                participants: [senderId, receiverId]
+            })
+        }
+        const newMessage = await new Message({
+            senderId,
+            receiverId,
+            message
+        })
+        if(newMessage){
+            conversation.messages.push(newMessage._id);
+        }
+        await Promise.all([conversation.save(), newMessage.save()]);
+        res.status(200).json({conversation})
     }catch(err){
         console.log("Error during sendMessage:", err.message);
         res.status(500).json({error: err.message});
     }
-}
+} 
--- chatApp/backend/middleware/protectRoute.js
+++ chatApp/backend/middleware/protectRoute.js
@@ -9,4 +9,4 @@ export const protectRoute = (req, res, next) => {
     const decoded = jwt.verify(token, process.env.SECRET);
     req.userId = decoded.userId;
     next();
-}
+} 
